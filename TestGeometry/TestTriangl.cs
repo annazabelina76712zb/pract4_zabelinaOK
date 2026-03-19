55ded6fd12fc437c54796ff9d14a595310898d5c
annazabelina76712zb <anna.zabelina07@gmail.com>
2026-03-19 20:05:17 +0500

    ADD test

---
+++ TestGeometry/TestTriangl.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassGeometry;
+namespace TestGeometry
+{
+    public class TestTriangleCI
+    {
+        [Fact]
+        public void CreateTriangle() 
+        {
+            ClassGeometry.Triangle tri = new ClassGeometry.Triangle();
+            Assert.NotNull(tri);
+        }
+
+        [Fact]
+        public void TriangleArea_Check()
+        {
+            Triangle tri = new Triangle();
+            tri.SideA = 5;
+            tri.SideB = 5;
+            tri.SideC = 5;
+
+            Assert.Equal(10.825317547305483, tri.Area());
+        }
+
+        [Fact]
+        public void TriangleArea_CheckEx()
+        {
+            Triangle tri = new Triangle();
+            tri.SideA = -5;
+            tri.SideB = 5;
+            tri.SideC = 5;
+
+            var ex = Assert.Throws<ArgumentException>(() => tri.Area());
+
+            Assert.Contains("error!", ex.Message.ToString());
+        }
+
+        [Fact]
+        public void TrianglePerimetre_Check()
+        {
+            Triangle tri = new Triangle();
+            tri.SideA = 5;
+            tri.SideB = 5;
+            tri.SideC = 5;
+
+            Assert.Equal(15, tri.Perimeter());
+        }
+
+        [Fact]
+        public void TrianglePerimetre_CheckEx()
+        {
+            Triangle tri = new Triangle();
+            tri.SideA = -5;
+            tri.SideB = 5;
+            tri.SideC = 5;
+
+            var ex = Assert.Throws<ArgumentException>(() => tri.Perimeter());
+
+            Assert.Contains("error!", ex.Message.ToString());
+        }
+    }
+}
